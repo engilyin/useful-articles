0c882d5236a382bb86589d460313b31d9b80125b
Alex Ilin <engilyin@gmail.com>
2022-11-06 11:05:26 -0800

    test signoff

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/'
+    redirectTo: '/home'
   },
 
   {
--- frontend/src/app/screens/test/test/test.component.ts
+++ frontend/src/app/screens/test/test/test.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { ThemeService } from '@root/app/services/sys/theme/theme.service';
+import { AppState } from '@root/app/store/app.state';
+import * as SessionActions from '@store/session/session.actions';
 
 @Component({
   selector: 'ua-test',
@@ -8,7 +11,7 @@ import { ThemeService } from '@root/app/services/sys/theme/theme.service';
 })
 export class TestComponent implements OnInit {
 
-  constructor(private themeService: ThemeService) { }
+  constructor(private readonly store: Store<AppState>, private themeService: ThemeService) { }
 
   ngOnInit(): void {
   }
@@ -20,5 +23,10 @@ export class TestComponent implements OnInit {
 
   darkTheme() {
     this.themeService.switchTheme('dark');
+  }
+
+
+  doSignoff() {
+    this.store.dispatch(SessionActions.signoff());
   }
 }
